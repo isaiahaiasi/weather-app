29d8618efab00faeea755e0b23ab4781b342b4b2
isaiahaiasi <isaiah.althouse@gmail.com>
2021-03-01 00:48:37 -0700

    Setup project

---
+++ src/postload.js
@@ -0,0 +1,3 @@
+const div = document.createElement('div');
+div.textContent = 'hello again';
+document.body.appendChild(div);
---
+++ src/preload.js
@@ -0,0 +1,3 @@
+const div = document.createElement('div');
+div.textContent = 'hello webpack';
+document.body.appendChild(div);
